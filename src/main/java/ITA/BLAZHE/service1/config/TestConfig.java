99b1d2b09ede5efbcee4436fee49d2f11583aa95
Blazhe_Manev <manev.blazhe@student.um.si>
2024-03-11 19:38:42 +0100

    test added

---
+++ src/main/java/ITA/BLAZHE/service1/config/TestConfig.java
@@ -0,0 +1,18 @@
+package ITA.BLAZHE.service1.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+}
+
