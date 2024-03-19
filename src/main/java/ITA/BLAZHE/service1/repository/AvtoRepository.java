643d8df6f535b91fd56e1db4a0eb310575fd1d71
Blazhe_Manev <manev.blazhe@student.um.si>
2024-03-19 15:02:30 +0100

    edit files

--- src/main/java/ITA/BLAZHE/service1/repository/AvtoRepository.java
+++
@@ -1,9 +0,0 @@
-package ITA.BLAZHE.service1.repository;
-
-
-import ITA.BLAZHE.service1.model.Avto;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AvtoRepository extends MongoRepository<Avto, String> {
-}
-
